b9ecb30598a59ac14c4f874cfb8fd3915c888f09
Starinin Andrey <starininandrey@gmail.com>
2022-08-18 17:25:10 +0300

    Add insert_test_data.sql

---
+++ StoreDemo.SQL/insert_test_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tab_types (type_name) VALUES ('computer');
+INSERT INTO tab_types (type_name) VALUES ('car');
+
+
+INSERT INTO tab_products (product_name, type_id) VALUES ('Apple', 1);
+INSERT INTO tab_products (product_name, type_id) VALUES ('PC', 1);
+INSERT INTO tab_products (product_name, type_id) VALUES ('BMW', 2);
+INSERT INTO tab_products (product_name, type_id, is_active) VALUES ('Lada', 2, 0);
